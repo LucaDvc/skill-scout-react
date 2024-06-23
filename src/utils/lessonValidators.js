3065d2c39d758f4d9aa42d12776379ded2aae5a1
LucaDvc <lukadvc1@gmail.com>
2024-06-23 18:08:50 +0300

    added lesson steps edit validation

--- src/utils/lessonValidators.js
+++ src/utils/lessonValidators.js
@@ -17,7 +17,7 @@ export const validateLesson = async (title, steps) => {
 
   const pyodide = await loadPyodideSingleton();
 
-  if (!(await validateTextProblems(steps, pyodide))) {
+  if (!(await validateSteps(steps, pyodide))) {
     return false;
   }
 
@@ -49,24 +49,143 @@ is_valid_regex(${JSON.stringify(regex)})`;
   }
 };
 
-const validateTextProblems = async (steps, pyodide) => {
+const validateSteps = async (steps, pyodide) => {
   let isValid = true;
   for (const step of steps) {
-    if (step.type === 'text_problem') {
-      if (!step.statement || !step.correct_answer || !step.title) {
-        isValid = false;
-        toast.error(
-          `Statement, title and correct answer are required for text problems (step ${step.order}).`
-        );
-      }
-      if (step.allow_regex) {
-        const isValidRegex = await validateRegex(pyodide, step.correct_answer);
-        if (!isValidRegex) {
+    switch (step.type) {
+      case 'text_problem':
+        if (!(await validateTextProblem(step, pyodide))) {
           isValid = false;
-          toast.error(`Invalid regex pattern in text problem (step ${step.order}).`);
         }
-      }
+        break;
+      case 'text':
+        if (!validateTextStep(step)) {
+          isValid = false;
+        }
+        break;
+      case 'quiz':
+        if (!validateQuizStep(step)) {
+          isValid = false;
+        }
+        break;
+      case 'codechallenge':
+        if (!validateCodeChallengeStep(step)) {
+          isValid = false;
+        }
+        break;
+      case 'sorting_problem':
+        if (!validateSortingProblem(step)) {
+          isValid = false;
+        }
+        break;
+      default:
+        console.error(`Unknown step type: ${step.type}`);
+        isValid = false;
     }
+  }
+  return isValid;
+};
+
+const validateTextProblem = async (step, pyodide) => {
+  let isValid = true;
+  if (!step.statement || !step.correct_answer || !step.title) {
+    isValid = false;
+    toast.error(
+      `Statement, title and correct answer are required for text problems (step ${step.order}).`
+    );
+  }
+  if (step.allow_regex) {
+    const isValidRegex = await validateRegex(pyodide, step.correct_answer);
+    if (!isValidRegex) {
+      isValid = false;
+      toast.error(`Invalid regex pattern in text problem (step ${step.order}).`);
+    }
+  }
+  return isValid;
+};
+
+const validateTextStep = (step) => {
+  let isValid = true;
+  if (!step.text) {
+    isValid = false;
+    toast.error(`Content is required for text steps (step ${step.order}).`);
+  }
+
+  return isValid;
+};
+
+const validateQuizStep = (step) => {
+  let isValid = true;
+  if (!step.question) {
+    isValid = false;
+    toast.error(`Question is required for quiz steps (step ${step.order}).`);
+  }
+  if (step.quiz_choices.length < 2) {
+    isValid = false;
+    toast.error(`At least two options are required for quiz steps (step ${step.order}).`);
+  }
+  if (!step.quiz_choices.some((choice) => choice.correct)) {
+    isValid = false;
+    toast.error(
+      `At least one correct option is required for quiz steps (step ${step.order}).`
+    );
+  }
+  return isValid;
+};
+
+const validateCodeChallengeStep = (step) => {
+  let isValid = true;
+  if (!step.title || !step.description || !step.initial_code || !step.language_id) {
+    isValid = false;
+    toast.error(
+      `Title, description, initial code and language are required for code challenge steps (step ${step.order}).`
+    );
+  }
+
+  // at least 5 test cases, none should be empty and duplicates are not allowed
+  if (step.test_cases.length < 5) {
+    isValid = false;
+    toast.error(
+      `At least 5 test cases are required for code challenge steps (step ${step.order}).`
+    );
+  }
+  const testCases = new Set();
+  for (const testCase of step.test_cases) {
+    if (!testCase.input || !testCase.expected_output) {
+      isValid = false;
+      toast.error(
+        `Input and output are required for test cases in code challenge steps (step ${step.order}).`
+      );
+      break;
+    }
+    const key = `${testCase.input} ${testCase.expected_output}`;
+    if (testCases.has(key)) {
+      isValid = false;
+      toast.error(
+        `Duplicate test cases are not allowed in code challenge steps (step ${step.order}).`
+      );
+      break;
+    }
+    testCases.add(key);
+  }
+
+  return isValid;
+};
+
+const validateSortingProblem = (step) => {
+  let isValid = true;
+  if (!step.statement || !step.title) {
+    toast.error(
+      `Statement and title are required for sorting problems (step ${step.order}).`
+    );
+    isValid = false;
+  }
+  // at least 2 options
+  if (step.options.length < 2) {
+    toast.error(
+      `At least two options are required for sorting problems (step ${step.order}).`
+    );
+    isValid = false;
   }
   return isValid;
 };
